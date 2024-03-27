56555843ae4cbaf0861e350d91de8e39fab4dc59
Hsin-Yeh Wu <thankyouyou06@gmail.com>
2024-03-27 14:26:58 +0800

    tdms2root update

--- SNSPD_Laser_measurements/python/tdms2root.py
+++ SNSPD_Laser_measurements/python/tdms2root.py
@@ -9,7 +9,7 @@ import errno
 import os
 
 # User defined functions
-from tdmsUtils import *
+from utils.tdmsUtils import *
 
 import argparse
 parser = argparse.ArgumentParser(description='')
@@ -18,8 +18,60 @@ parser.add_argument('--outputDir','-d',default="./output_root",type=str,help='ou
 parser.add_argument('--report','-r',default=100,type=int,help='report every x events')
 args = parser.parse_args()
 
-if __name__ == "__main__":
+def Convert_Single_TDMS(in_filename):
+    with TdmsFile.open(in_filename) as tdms_file:
+        baseName = in_filename.rsplit('/',1)[1].split('.tdms')[0]
+        # Read Meta Data (Basic information)
+        metadata = tdms_file.properties
+        metadata_df = pd.DataFrame(metadata.items(), columns=['metaKey', 'metaValue'])
+        print(metadata_df)
+        totalEvents = int(metadata_df.loc[metadata_df['metaKey'] == 'Total Events', 'metaValue'].iloc[0])
+        recordlength = int(metadata_df.loc[metadata_df['metaKey'] == 'record length', 'metaValue'].iloc[0])
+        vertical_range = float(metadata_df.loc[metadata_df['metaKey'] == 'vertical range Sig', 'metaValue'].iloc[0])
+        # Write metadata to json file
+        metadata_df.to_json(f'{args.outputDir}/{baseName}.json',orient="records",lines=True)
+        # Write metadata to txt file
+        metadata_df = metadata_df.reset_index()  # make sure indexes pair with number of rows
+        with open(f'{args.outputDir}/{baseName}.txt', 'w') as txtFile:
+            for index, row in metadata_df.iterrows():
+                txtFile.write(f"{row['metaKey']} : {row['metaValue']}\n")
+        # Create output tree
+        outtree = ROOT.TTree("SNSPD_data", "SNSPD_data")
+        chSig = array( 'f', [ 0 ] * recordlength)
+        chTrig = array( 'f', [ 0 ] * recordlength)
+        outtree.Branch('chSig',chSig,"chSig[%d]/F" %(recordlength))
+        outtree.Branch('chTrig',chTrig,"chTrig[%d]/F" %(recordlength))
+        # Read Groups and Channels
+        Read_Groups_and_Channels(tdms_file)
+        chSig_total = tdms_file['ADC Readout Channels']['chSig']
+        chTrig_total = tdms_file['ADC Readout Channels']['chTrig']
+        # chSig_average = np.zeros(avg_buffer)
+        # Start Loop
+        print (f"==========Start Looping==========")
+        for event, chunk in enumerate(tdms_file.data_chunks()):
+            # Loop progress
+            if ((event+1)%args.report==0): print (f"==========Processing {event}/{totalEvents} event==========")
+            # Skip chunk larger than totalEvents
+            if (event > totalEvents-1): break
+            # Read data into np array
+            chSig_chunk = chunk['ADC Readout Channels']['chSig']._data()
+            chTrig_chunk = chunk['ADC Readout Channels']['chTrig']._data()
+            # copy to tree branch
+            for i in range(0, recordlength):
+                chSig[i] = chSig_chunk[i];
+                chTrig[i] = chTrig_chunk[i];
+            # Fill Tree
+            outtree.Fill()
+    # Output
+    rootFileName = f"{args.outputDir}/{baseName}.root"
+    print(f"output file: {rootFileName}")
+    rootFile = ROOT.TFile.Open(rootFileName,"RECREATE")
+    rootFile.cd()
+    outtree.Write()
+    rootFile.Close()
 
+
+if __name__ == "__main__":
     # make outputDir
     try:
         os.makedirs(args.outputDir)
@@ -28,53 +80,6 @@ if __name__ == "__main__":
             print('output directory exists.')
         else:
             raise
-
-    for i, in_filename in enumerate(args.in_filenames):
-        print('Processing %s (%d/%d)'% (in_filename, i, len(args.in_filenames)))
-        if (in_filename.find('txt')!=-1): baseName = in_filename.rsplit('/',1)[1].split('.txt')[0]
-        elif (in_filename.find('tdms')!=-1): baseName = in_filename.rsplit('/',1)[1].split('.tdms')[0]
-        with TdmsFile.open(in_filename) as tdms_file:
-            # Read Meta Data (Basic information)
-            metadata = tdms_file.properties
-            metadata_df = pd.DataFrame(metadata.items(), columns=['metaKey', 'metaValue'])
-            print(metadata_df)
-            totalEvents = int(metadata_df.loc[metadata_df['metaKey'] == 'Total Events', 'metaValue'].iloc[0])
-            recordLength = int(metadata_df.loc[metadata_df['metaKey'] == 'record length', 'metaValue'].iloc[0])
-            # Read Groups and Channels
-            Read_Groups_and_Channels(tdms_file)
-
-            # Write metadata to json file
-            metadata_df.to_json(f'{args.outputDir}/{baseName}.json',orient="records",lines=True)
-            # Write metadata to txt file
-            metadata_df = metadata_df.reset_index()  # make sure indexes pair with number of rows
-            with open(f'{args.outputDir}/{baseName}.txt', 'w') as txtFile:
-                for index, row in metadata_df.iterrows():
-                    txtFile.write(f"{row['metaKey']} : {row['metaValue']}\n")
-
-
-            ch1 = array( 'f', [ 0 ] * recordLength)
-            ch2 = array( 'f', [ 0 ] * recordLength)
-            t = ROOT.TTree("SNSPD_data", "SNSPD_data")
-            t.Branch('ch1',ch1,"ch1[%d]/F" %(recordLength))
-            t.Branch('ch2',ch2,"ch2[%d]/F" %(recordLength))
-
-            for event, chunk in enumerate(tdms_file.data_chunks()):
-                # Loop progress
-                if ((event+1)%args.report==0): print (f"==========Processing {event}/{totalEvents} event==========")
-                # Skip chunk larger than totalEvents
-                if (event > totalEvents-1): break
-                # Read ch1 into np array
-                ch1_chunk = chunk['ADC Readout Channels']['ch1']._data()
-                ch2_chunk = chunk['ADC Readout Channels']['ch2']._data()
-
-                for i in range(0, recordLength):
-                    ch1[i] = ch1_chunk[i];
-                    ch2[i] = ch2_chunk[i];
-                t.Fill()
-
-            # Output
-            rootFileName = f"{args.outputDir}/{baseName}.root"
-            rootFile = ROOT.TFile.Open(rootFileName,"RECREATE")
-            rootFile.cd()
-            t.Write()
-            rootFile.Close()
+    # Loop the input tdms files
+    for in_filename in args.in_filenames:
+        Convert_Single_TDMS(in_filename)
