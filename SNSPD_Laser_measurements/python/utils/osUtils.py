7d60c810c0d6859ffedb38825f5bdbbfcc9d92e1
Hsin-Yeh Wu <thankyouyou06@gmail.com>
2024-04-10 11:34:15 +0800

    update

---
+++ SNSPD_Laser_measurements/python/utils/osUtils.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import os
+import warnings
+import os
+import errno
+
+
+def create_755(path):
+    createDir(path)
+    os.chmod(path,0o755)
+
+def rmDir(path):
+    os.rmdir(path)
+
+def createDir(Dir):
+    try:
+        os.makedirs(Dir)
+    except OSError as e:
+        if e.errno == errno.EEXIST:
+            print(f'{Dir} exists.')
+        else:
+            raise
