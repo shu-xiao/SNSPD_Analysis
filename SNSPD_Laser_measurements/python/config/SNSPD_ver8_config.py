1704749dce92296e4adb35b22a39b06bb98417bf
Hsin-Yeh Wu <thankyouyou06@gmail.com>
2024-04-12 21:00:44 +0800

    update

---
+++ SNSPD_Laser_measurements/python/config/SNSPD_ver8_config.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+DEBUG = False
+DISPLAY = False
+
+
+########## 20240223 Prima data ##########
+NpulsePerTrigger=1
+# Define signal region
+Pulse_startT     =  67 #200 #215
+Pulse_endT       =  125 #215 #245
+# Define rising and falling separation
+Pulse_rise_endT     = 75 #200 #215
+# Define pre-pulse (sideband) region
+prePulse_startT  =  10
+prePulse_endT    =  60
+# Define post-pulse (sideband) region
+postPulse_startT =  140
+postPulse_endT   =  180
+
+# Sideband pre-selection
+cut_preRange = 0.11
+cut_posRange = 10
+cut_preStd = 0.021
+cut_posStd = 10
+
+# Pulse selection
+cut_pulseRange = 0.05
+
+# FFT
+freq_steps = 10000
+
+totalTreeEvents = 10000
+avgCount = 1000
+threshold = 0.1
