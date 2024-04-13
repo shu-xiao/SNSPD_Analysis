b1bd255cd4909c79e36406d441f0de2f170f3052
Hsin-Yeh Wu <thankyouyou06@gmail.com>
2024-04-13 19:32:17 +0800

    update

--- SNSPD_Laser_measurements/python/NIPXIE/plot_multiHistograms.py
+++ SNSPD_Laser_measurements/python/NIPXIE/plot_multiHistograms.py
@@ -234,7 +234,7 @@ def multi_histo_canvas(bias,histos):
         stat.SetOptStat(1101)
         stat.SetY1NDC(0.6)
         stat.SetX1NDC(0.6)
-        stat.SetStatFormat(6.2)
+        stat.SetStatFormat("6.4g")
         histos[b].Draw()
     c_multi.SaveAs("test.png")
 
