69ab9d8f669d4e77f82759209c8c1a2b947135fb
Hsin-Yeh Wu <thankyouyou06@gmail.com>
2024-04-14 18:51:18 +0800

    update

--- SNSPD_Laser_measurements/python/utils/plotUtils.py
+++ SNSPD_Laser_measurements/python/utils/plotUtils.py
@@ -137,7 +137,7 @@ def fit_histo_gaus(hist, rangemin, rangemax, mean_min, mean_max, sigma_min, sigm
     fit.SetParLimits(2,sigma_min,sigma_max)
     fit.SetParameter(2,sigma_min);
     fit.FixParameter(2,sigma_min);
-    hist.Fit("fit",'BR')
+    hist.Fit("fit",'BRQ')
     # Draw hist
     hist.SetTitle(title)
     hist.GetXaxis().SetTitle(xTitle)
