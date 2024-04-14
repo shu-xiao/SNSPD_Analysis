24b0d96c0eee6f47d4bc405d41d8ea7cb1e29137
Hsin-Yeh Wu <thankyouyou06@gmail.com>
2024-04-14 18:23:00 +0800

    update

--- SNSPD_Laser_measurements/python/NIPXIE/plot_multiHistograms.py
+++ SNSPD_Laser_measurements/python/NIPXIE/plot_multiHistograms.py
@@ -314,7 +314,7 @@ def calculate_tree():
         # h_pulse_fall_range_rebin2 = rebin(h_pulse_fall_range_rebin1,f'{basename}_rebin',"pulse_range (V)",f"Event/{(range_max-range_min)/nbin:.4f}V",plotDir,"h_pulse_fall_range_rebin2",True)
         # h_pulse_fall_range_rebin3 = rebin(h_pulse_fall_range_rebin2,f'{basename}_rebin',"pulse_range (V)",f"Event/{(range_max-range_min)/nbin:.4f}V",plotDir,"h_pulse_fall_range_rebin3",True)
         # Fit
-        fit_histo_gaus(h_pulse_fall_range, -0.1, 0.012, f"fit_pulse_fall_range_{basename}", 'pulse_range (V)', "", f"{plotDir}/fit_pulse_fall_range.png")
+        fit_histo_gaus(h_pulse_fall_range, -0.1, 0.12, f"fit_pulse_fall_range_{basename}", 'pulse_range (V)', "", f"{plotDir}/fit_pulse_fall_range.png")
         # Calculate
         eff = h_eff.Integral()/intree.GetEntries()
         pre_range = h_pre_range.GetMean()
