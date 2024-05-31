f4e75ffbfbad18e049797ac0bf99f6847537158a
Hsin-Yeh Wu <thankyouyou06@gmail.com>
2024-05-31 12:02:33 +0800

    update

--- SNSPD_Laser_measurements/python/IV/plotIV_residual.py
+++ SNSPD_Laser_measurements/python/IV/plotIV_residual.py
@@ -42,8 +42,8 @@ def read_file(in_filename,subset):
     return subset_df, Vars
 
 def bare():
-    # Bare_file="/wk_cms3/wuhsinyeh/SNSPD/SNSPD_rawdata/Bare/IV_Current_Source/Ch0/Bare_Ch0_300K_20240524_1310.txt"
-    Bare_file="/Volumes/HV620S/SNSPD/SNSPD_rawdata/Bare/IV_Current_Source/Ch0/Bare_Ch0_300K_20240524_1310.txt"
+    Bare_file="/wk_cms3/wuhsinyeh/SNSPD/SNSPD_rawdata/Bare/IV_Current_Source/Ch0/Bare_Ch0_300K_20240524_1310.txt"
+    # Bare_file="/Volumes/HV620S/SNSPD/SNSPD_rawdata/Bare/IV_Current_Source/Ch0/Bare_Ch0_300K_20240524_1310.txt"
     df_bare, Vars_bare = read_file(Bare_file, 1000)
     cubic_spline = CubicSpline(df_bare['Currents'], df_bare['Resists'])
     return cubic_spline
@@ -84,7 +84,7 @@ if __name__ == '__main__':
     resists, temps=[], []
     bare_spline = bare()
     for i, in_filename in enumerate(args.in_filenames):
-        df, Vars = read_file(in_filename, 1000)
+        df, Vars = read_file(in_filename, 2000)
         resists.append(residual(df,bare_spline))
         temps.append(Vars["temp"])
 
@@ -92,3 +92,10 @@ if __name__ == '__main__':
     createDir(plotDir)
     # sort_temp(temps,resists)
     plot(temps, resists, r"Bias Current ($\mu$A)", r"Resistance (k$\Omega$)", plotDir, "IR_residual")
+
+
+# 0.38    127     127.3   0.3
+# 0.83:   32.1    37.3    5.2
+# 0.85:   29.2    34.4    5.2
+# 0.92:   13.6    22.5    8.9
+# 0.96:   6.6     15.3    8.7
